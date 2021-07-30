46045cfb59248488e7c87a3825b9ffb299403454
ye-gang-jjang <yegang13209@naver.com>
2021-07-30 11:32:49 +0900

    돔을 이용해 HTML 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,5 +1,15 @@
 "use strict";
 
-console.log("hello");
-console.log("나예강")
-console.log("나예강")
+const id = document.querySelector("#email"),   //querySelector 는 '질의 선택자' 두번째 파라미터로 들어오는 선택자로 값을 가져올 수 있음.
+    psword = document.querySelector("#psword"),
+    loginBtn = document.querySelector("button");
+
+
+loginBtn.addEventListener("click", login);   //click했을 때 login(함수)
+
+function login() {
+    const req = {
+        id: id.value,
+        psword: psword.value,
+    };
+}
